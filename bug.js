8e5aa9a27dcee9a12886a61ed8dcbf9856616bf8
BugHunter-X <askzenai@gmail.com>
2024-12-08 04:42:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase SDK may throw an error if you try to access a database reference before it has fully initialized. This can happen if you try to perform a database operation in your app's main function or before the Firebase app has finished starting up.
